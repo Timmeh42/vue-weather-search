eed5ba39a1ad632f3350213e2d19f11d71842937
timmeh42 <tmeier42+github@gmail.com>
2022-06-10 15:32:32 +0200

    initial api communication and display

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <form @submit.prevent="searchLocation(locationName)">
+            <input type="search" v-model="locationName">
+            <button>Search</button>
+        </form>
+        <table v-if="locationResults.length">
+            <thead>
+                <tr>
+                    <th>Flag</th>
+                    <th>Place</th>
+                    <th>State</th>
+                    <th>Country</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                    v-for="location of locationResults"
+                    :key="`${location.lon},${location.lat}`"
+                >
+                    <td>{{ countries[location.country].emoji }}</td>
+                    <td><button @click="loadForecast({lat: location.lat, lon: location.lon})">{{ location.name }}</button></td>
+                    <td>{{ location.state }}</td>
+                    <td>{{ countries[location.country].name }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div v-if="forecastResult">
+            <img :src="`http://openweathermap.org/img/wn/${forecastResult.current.weather[0].icon}@2x.png`" />
+            Temperature: {{ forecastResult.current.main.temp }}c
+            Humidity: {{ forecastResult.current.main.humidity }}%
+            Wind Speed: {{ forecastResult.current.wind.speed }}m/s
+            Cloud Cover: {{ forecastResult.current.clouds.all }}%
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { countries } from 'country-data';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data: function () {
+        return {
+            locationName: '',
+            locationResults: [],
+            forecastResult: undefined,
+            countries,
+        }
+    },
+    methods: {
+        searchLocation: function (locationName) {
+            fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${locationName}&limit=3&appid=${process.env.VUE_APP_API_KEY}`)
+                .then((response) => response.json())
+                .then((searchResults) => {
+                    this.locationResults = searchResults;
+                });
+        },
+        loadForecast: function ({lon, lat}) {
+            Promise.all([
+                    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.VUE_APP_API_KEY}`).then((res) => res.json()),
+                    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.VUE_APP_API_KEY}`).then((res) => res.json()),
+                ])
+                .then((responses) => ({
+                    forecast: responses[0],
+                    current: responses[1],
+                }))
+                .then((res) => {
+                    this.forecastResult = res;
+                });
+        }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
